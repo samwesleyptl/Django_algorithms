f20ba97c1c347b2f25ad37dbb26328eb19e64877
samwesleyptl <sam.wesley168@gmail.com>
2024-09-16 20:30:34 +0530

    Update Algorithm

--- ac_energy_optimizer/ac_data/algorithms.py
+++ ac_energy_optimizer/ac_data/algorithms.py
@@ -1,12 +1,19 @@
 # ac_data/algorithms.py
-def optimize_energy(room_temp, ac_temp, humidity):
-    # Simple logic to adjust AC temperature and calculate energy-saving factor
-    if room_temp > 25 and humidity > 60:
-        new_ac_temp = ac_temp + 1
-    elif room_temp < 20:
-        new_ac_temp = ac_temp - 1
+def optimize_energy(room_temp, ac_temp):
+    if ac_temp < 26:
+        if room_temp > 25:
+            mode = "Cooling"
+            setpoint = room_temp + 1
+        elif room_temp < 25:
+            mode = "Cooling"
+            setpoint = ac_temp + 1
+        else:
+            mode = "Cooling"
+            setpoint = ac_temp
     else:
-        new_ac_temp = ac_temp
+        mode = "Fan"
+        setpoint = None  # Fan mode doesn't require a setpoint temperature
 
-    energy_saving_factor = max(0, 1 - abs(new_ac_temp - ac_temp) * 0.05)
-    return new_ac_temp, energy_saving_factor
+
+    energy_saving_factor = max(0, 1 - abs(setpoint - ac_temp) * 0.05)
+    return setpoint, energy_saving_factor
